03d865ea9391cf1858ede70cbc828516c9946ad0
yamaan93 <yamaan93@gmail.com>
2017-11-06 10:43:05 -0500

    Initial commit

---
+++ _7_segment.ino
@@ -0,0 +1,27 @@
+int top =  10;
+int topRight =11;
+int topLeft=9;
+int middle = 8;
+int bottomRight =6;
+int bottom = 5;
+int bottomLeft = 4;
+int up =3;
+int down =4;
+int array screen[DIM_0_0] [DIM_1_11] = {
+  {
+  
+}
+
+
+void setup() {
+  for(int i=4; i<12; i++){
+    pinMode(i,OUTPUT);
+  }
+  pinMode(3,INPUT);
+  pinMode(4,INPUT);
+}
+
+void loop() {
+  
+}
+  
