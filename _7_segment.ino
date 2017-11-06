a0eecdbcf1c16b24e32674d15f9e4a8eafcf93ff
yamaan93 <yamaan93@gmail.com>
2017-11-06 10:53:14 -0500

    started on 2 d array

--- _7_segment.ino
+++ _7_segment.ino
@@ -6,28 +6,36 @@ int E = 6;
 int F = 7;
 int G = 8;
 int DP = 9;
-int [][] 7-state ={
-                    
+int []segment{A, B, C, D, E, F, G, DP}
+int [][] 7 - state = {
+  {A, F, G, E, D}
+  {F, E, -1, -1, -1}
+  {A,B,G,E,D}
+  {
   
+
+
+
+
 }
 
-  
+
 }
 
 
 void setup() {
-  for(int i=4; i<12; i++){
-    pinMode(i,OUTPUT);
+  for (int i = 4; i < 12; i++) {
+    pinMode(i, OUTPUT);
   }
-  pinMode(3,INPUT);
-  pinMode(4,INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
 }
 
 void loop() {
   int number = 0;
-  switch (number){
+  switch (number) {
 
-    case 0: 
-  
-}
-  
+    case 0:
+
+  }
+
