9721c1c84d8df77f5ca58e0a59d87334c0d63121
YAMAANS-LAPTOP\Yamaan <yamaan93@gmail.com>
2017-11-06 19:15:43 -0500

    v0.4 fixed the array,

--- _7_segment.ino
+++ _7_segment.ino
@@ -7,7 +7,7 @@ int E = 6;
 int F = 7;
 int G = 8;
 int DP = 9;
-unsigned int []segment{A, B, C, D, E, F, G, DP}
+int segment [] {A, B, C, D, E, F, G, DP};
 // Declare 2D array
 
 
@@ -23,27 +23,30 @@ void setup() {
 }
 
 void loop() {
-unsigned int[7][7] segment{
+  int segment [][7] ={
 
-  {A, F, G, E, D,-1,-1}
-  {A,B,C,D,E,F,-1}
-  {F, E, -1, -1, -1,-1,-1}
-  {A,B,G,E,D,-1,-1}
-  {A,B,G,C,D,-1,-1}
-  {F,G,B,C,-1,-1,1}
-  {A,F,G,C,D,-1,-1}
-  {A,F,E,D,C,G.-1}
-  {A,B,C,-1,-1,-1,-1}
-  {A,B,C,D,E,F,G,}
-  {A,B,G,F,C,-1,-1}
-  {A, F, G, E, D,-1,-1}
-}
-}
+    {A, F, G, E, D, -1, -1},
+    {A, B, C, D, E, F, -1},
+    {F, E, -1, -1, -1, -1, -1},
+    {A, B, G, E, D, -1, -1},
+    {A, B, G, C, D, -1, -1},
+    {F, G, B, C, -1, -1, -1},
+    {A, F, G, C, D, -1, -1},
+    {A, F, E, D, C, G, -1},
+    {A, B, C, -1, -1, -1, -1},
+    {A, B, C, D, E, F, G,},
+    {A, B, G, F, C, -1, -1},
+    {A, F, G, E, D, -1, -1}
+  };
+  
+
+
   int number = -1;
   switch (number) {
 
-    case -1: digitalWrite([1][j]},1);
-      
+ 
+
 
   }
+}
 
